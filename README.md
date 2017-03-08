382ce0e21d26182859d0743ac2c1598b38ee2d96
Jan Schulte <j.schulte@52north.org>
2017-03-08 12:10:36 +0100

    add js file

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ A very simple GitHub powered content delivery network for common static files, f
 ## Current Files
 
 * License Mappings file for maven-notice-plugin > http://52north.github.io/cdn/licenses/license-mappings.xml
+* JavaScript file for handling the project github page on the homepage > http://52north.github.io/cdn/homepage/homepage-markdown-parser.js
 
 ## Documentation
 
 Visit https://wiki.52north.org/bin/view/Documentation/BestPracticeLicenseManagementInSoftwareProjects to find out how to use these files.
-
---
+++ homepage/homepage-markdown-parser.js
@@ -0,0 +1,40 @@
+  var content = {};
+  jQuery(document).ready(function() {
+      var menuEntries = mdMenuConfig.menuEntries;
+
+      // create content entries
+      jQuery.get(mdMenuConfig.rawReadmeMD, function(markdown) {
+          if (markdown) {
+              for (i = 0; i < menuEntries.length; i++) {
+                  content[menuEntries[i].key] = getSection(markdown, menuEntries[i].githubTitle);
+              }
+          }
+
+          // create menu
+          var menuElem = jQuery.find('dl.level1')[0];
+          jQuery(menuElem).empty();
+          for (i = 0; i < menuEntries.length; i++) {
+              if (content[menuEntries[i].key]) {
+                  addMenuEntry(menuEntries[i], menuElem);
+              }
+          }
+
+          // load first entry section
+          loadSection(menuEntries[0].key);
+      });
+  });
+
+  function addMenuEntry(entry, div) {
+      jQuery(div).append('<dt class="level1 parent opened forceopened first"><span class="outer"><span class="inner"><a onclick="loadSection(\'' + entry.key + '\')"><span>' + entry.label + '</span></a></span></span></dt>');
+      jQuery(div).append('<dd class="level1 notparent last"></dd>');
+  }
+
+  function getSection(md, header) {
+      return md.substring(md.indexOf('\n', md.indexOf('## ' + header)), md.indexOf('\n## ', md.indexOf('## ' + header) + 2)).trim();
+  }
+
+  function loadSection(index) {
+      jQuery('#content').empty();
+      // content[index] = content[index].replace('&uuml;', '');
+      jQuery('#content').append(marked(content[index]));
+  }
